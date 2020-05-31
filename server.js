cd601e373090cab757586de8ad0e33ae10a909ff
Glitch (hello-sqlite) <none>
2020-05-31 16:31:30 +0000

    🌖🏏 Checkpoint ./server.js:16007407/1616 ./.glitch-assets:16007407/366 ./package.json:16007407/882 ./config.json:16007407/161 ./system/music.js:16007407/1394 ./commands/loop.js:16007407/665 ./lyrics.js:16007407/1314 ./commands/lyrics.js:16007407/1314 ./commands/np.js:16007407/558 ./commands/pause.js:16007407/708 ./commands/ping.js:16007407/209 ./commands/play.js:16007407/3189 ./commands/queue.js:16007407/3189 ./commands/resume.js:16007407/653 ./commands/skip.js:16007407/653 ./commands/stop.js:16007407/669

--- server.js
+++ server.js
@@ -1,111 +1,61 @@
-// server.js
-// where your node app starts
+const discord = require("discord.js")
+const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { TOKEN, PREFIX } = require("./config.json")
 
-// init project
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const fs = require("fs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+//CLIENT EVENTS
+client.on("ready", () => {
+  console.log('Ready TO play some soft songs')
+  client.user.setActivity("x!help | Musix")
+})
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+client.on("warn", info => console.log(info));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+client.on("error", console.error)
 
-// init sqlite db
-const dbFile = "./.data/sqlite.db";
-const exists = fs.existsSync(dbFile);
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(dbFile);
+//DEFINIING
+client.commands = new discord.Collection()
+client.prefix = PREFIX
+client.queue = new Map();
 
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-db.serialize(() => {
-  if (!exists) {
-    db.run(
-      "CREATE TABLE Dreams (id INTEGER PRIMARY KEY AUTOINCREMENT, dream TEXT)"
-    );
-    console.log("New table Dreams created!");
 
-    // insert default dreams
-    db.serialize(() => {
-      db.run(
-        'INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")'
-      );
-    });
-  } else {
-    console.log('Database "Dreams" ready to go!');
-    db.each("SELECT * from Dreams", (err, row) => {
-      if (row) {
-        console.log(`record: ${row.dream}`);
-      }
-    });
+//LETS LOAD ALL FILES
+const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"))
+for (const file of cmdFiles) {
+  const command = require(join(__dirname, "commands", file))
+  client.commands.set(command.name, command)
+} //LOADING DONE
+
+
+//WHEN SOMEONE MESSAGE
+client.on("message", message => {
+   if (message.author.bot) return;
+  if (!message.guild) return;
+  
+  if(message.content.startsWith(PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
+    
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/) //removing prefix from args
+    const command = args.shift().toLowerCase();
+    
+    if(!client.commands.has(command)) {
+      return;
+    } 
+    
+  try  { //TRY TO GET COMMAND AND EXECUTE
+      client.commands.get(command).execute(client, message, args)
+    } catch (err) { //IF IT CATCH ERROR
+      console.log(err)
+      message.reply("I am getting error on using this command")
+    }
+    
   }
+  
+  
 });
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(`${__dirname}/views/index.html`);
-});
 
-// endpoint to get all the dreams in the database
-app.get("/getDreams", (request, response) => {
-  db.all("SELECT * from Dreams", (err, rows) => {
-    response.send(JSON.stringify(rows));
-  });
-});
 
-// endpoint to add a dream to the database
-app.post("/addDream", (request, response) => {
-  console.log(`add to dreams ${request.body.dream}`);
 
-  // DISALLOW_WRITE is an ENV variable that gets reset for new projects
-  // so they can write to the database
-  if (!process.env.DISALLOW_WRITE) {
-    const cleansedDream = cleanseString(request.body.dream);
-    db.run(`INSERT INTO Dreams (dream) VALUES (?)`, cleansedDream, error => {
-      if (error) {
-        response.send({ message: "error!" });
-      } else {
-        response.send({ message: "success" });
-      }
-    });
-  }
-});
-
-// endpoint to clear dreams from the database
-app.get("/clearDreams", (request, response) => {
-  // DISALLOW_WRITE is an ENV variable that gets reset for new projects so you can write to the database
-  if (!process.env.DISALLOW_WRITE) {
-    db.each(
-      "SELECT * from Dreams",
-      (err, row) => {
-        console.log("row", row);
-        db.run(`DELETE FROM Dreams WHERE ID=?`, row.id, error => {
-          if (row) {
-            console.log(`deleted row ${row.id}`);
-          }
-        });
-      },
-      err => {
-        if (err) {
-          response.send({ message: "error!" });
-        } else {
-          response.send({ message: "success" });
-        }
-      }
-    );
-  }
-});
-
-// helper function that prevents html/css/script malice
-const cleanseString = function(string) {
-  return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-};
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`);
-});
+//DONT DO ANYTHING WITH THIS TOKEN lol
+client.login(TOKEN)
