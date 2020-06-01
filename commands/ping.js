ad140f6206b6b2b38fb4ef5b1507ac6a768df070
Glitch (rythmjestergold) <none>
2020-06-01 13:24:15 +0000

    🌒🍟 Checkpoint ./commands/ping.js:16007407/2238 ./commands/skip.js:16007407/8

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,15 @@
 
 module.exports = {
-  name: "yardım",
+  name: "muzik",
   description: "Pinging the bot",
   execute(client, message) {
-  message.channel.send('Yardım Menüsüne Hoşgeldin :)')
-  message.channel.send('Yardım Menüsüne Hoşgeldin :)')
+  message.channel.send('<a:wuw:710205330625396868> | ``Rythm Gold Komut Menüsü``  ')
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -play = Müziği Başlatmanızı Sağlar ')
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -döngü = Müziği Tekrarlar ')
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -lyrics = Müziğin Sözlerini İnternetten Arar ')
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -şarkı-adı = Müziği Başlatmanızı Sağlar ')
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -durdur = Müziği Durdurmamızı Sağlar ')
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -sıra = Müzik Sırasını Göstermeyi Sağlar ')
     
 }
 }
