f3c25b70e6211f1e791126df03853a3954b39600
Glitch (rythmjestergold) <none>
2020-06-01 13:35:52 +0000

    🎿📌 Checkpoint ./commands/stop.js:16007407/37 ./commands/ping.js:16007407/590

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  name: "muzik",
+  name: "yardım",
   description: "Pinging the bot",
   execute(client, message) {
   message.channel.send('<a:wuw:710205330625396868> | ``Rythm Gold Komut Menüsü``  ')
@@ -10,6 +10,9 @@ module.exports = {
   message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -şarkı-adı = Müziği Başlatmanızı Sağlar ')
   message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -durdur = Müziği Durdurmamızı Sağlar ')
   message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -sıra = Müzik Sırasını Göstermeyi Sağlar ')
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -devam = Müziği Durdurduktan Sonra Başlatmayı Sağlar ')
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -atla = Müziği Atlar ')
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -leave = Müziği Bitirmeyi Sağlar ')
     
 }
 }
