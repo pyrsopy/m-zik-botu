8d5ea89dd1a2ab6a1d6d3802905e8a81c015822c
Glitch (rythmjestergold) <none>
2020-06-11 18:40:42 +0000

    👠🔑 Checkpoint ./commands/ping.js:16007407/395

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,8 @@ module.exports = {
   name: "yardım",
   description: "Pinging the bot",
   execute(client, message) {
-  message.channel.send('<a:wuw:710205330625396868> | ``Rythm Gold Komut Menüsü``  ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -play = Müziği Başlatmanızı Sağlar ')
+  .channel.send('<a:wuw:710205330625396868> | ``Rythm Gold Komut Menüsü``  ')
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -play = Müziği Başlatmanızı Sağlar /n dhhd')
   message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -döngü = Müziği Tekrarlar ')
   message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -lyrics = Müziğin Sözlerini İnternetten Arar ')
   message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -şarkı-adı = Müziği Başlatmanızı Sağlar ')
@@ -12,7 +12,13 @@ module.exports = {
   message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -sıra = Müzik Sırasını Göstermeyi Sağlar ')
   message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -devam = Müziği Durdurduktan Sonra Başlatmayı Sağlar ')
   message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -atla = Müziği Atlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -leave = Müziği Bitirmeyi Sağlar ')
-    
-}
+      if (args[0] == 'yap') {
+  const embedd = new discord.RichEmbed()
+  .setAuthor('Duyuru')
+  .setColor('RANDOM')
+  .setDescription(yazı)
+  .setTimestamp()
+  .setFooter(`${client.user.tag} | ${prefix}yardım`)
+  message.channel.send(embedd);
+  message.delete();
 }
