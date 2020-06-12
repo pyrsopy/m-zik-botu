fb8ab18d80d5848245d11cd599d0a9cc0d25614e
Glitch (rythmjestergold) <none>
2020-06-12 16:11:32 +0000

    👱🔦 Checkpoint ./commands/ping.js:16007407/3643

--- commands/ping.js
+++ commands/ping.js
@@ -3,5 +3,4 @@ module.exports = {
   name: "yardım",
   description: "Pinging the bot",
   execute(client, message) {
-  .channel.send('<a:wuw:710205330625396868> | ``Rythm Gold Komut Menüsü``  ')
-  message.channel.send('<a:rok2:710205094225
+  channel.send('<a:wuw:710205330625396868> | ``Rythm Gold Komut Menüsü``  ')
