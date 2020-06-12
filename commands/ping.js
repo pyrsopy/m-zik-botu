664353eaea33a86888fa3cd7a8ed4ba884ecd658
Glitch (rythmgold) <none>
2020-06-12 17:09:24 +0000

    📟🗺 Checkpoint ./commands/ping.js:16007407/1375

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,28 @@ module.exports = {
   name: "yardım",
   description: "Pinging the bot",
   execute(client, message) {
-  message.channel.send('Yardım Menüsüne Hoşgeldin :)')
-  message.channel.send('Yardım Menüsüne Hoşgeldin :)')
     
+ const embed = new Discord.RichEmbed()
+
+.setColor Kodunu Kod Embedken Renkli Yapabilirsiniz
+
+.setTitle Başlık
+
+.addField Açıklama
+
+.setDescription Açıklama Bittikten Sonraki Küçük Mesa
 }
 }
+
+const embed = new Discord.RichEmbed()
+
+
+ Kodunu Embed Yapmak İçin Kullanabilirisiniz!
+
+.setColor Kodunu Kod Embedken Renkli Yapabilirsiniz
+
+.setTitle Başlık
+
+.addField Açıklama
+
+.setDescription Açıklama Bittikten Sonraki Küçük Mesa
