7dfcece967ce2cd10be2ecd6ae3ad581d43203b9
Glitch (rythmgold) <none>
2020-06-12 17:05:24 +0000

    Rewound to commit fb8ab18d80d5848245d11cd599d0a9cc0d25614e + 0 edits

--- commands/ping.js
+++ commands/ping.js
@@ -3,16 +3,4 @@ module.exports = {
   name: "yardım",
   description: "Pinging the bot",
   execute(client, message) {
-  message.channel.send('<a:wuw:710205330625396868> | ``Rythm Gold Komut Menüsü``])
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -play = Müziği Başlatmanızı Sağlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -döngü = Müziği Tekrarlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -lyrics = Müziğin Sözlerini İnternetten Arar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -şarkı-adı = Müziği Başlatmanızı Sağlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -durdur = Müziği Durdurmamızı Sağlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -sıra = Müzik Sırasını Göstermeyi Sağlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -devam = Müziği Durdurduktan Sonra Başlatmayı Sağlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -atla = Müziği Atlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -leave = Müziği Bitirmeyi Sağlar ')
-    
-}
-}
+  channel.send('<a:wuw:710205330625396868> | ``Rythm Gold Komut Menüsü``  ')
