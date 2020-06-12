9df1b6f9924a0356e1d007a74787d6d32f530a3d
Glitch (rythmgold) <none>
2020-06-12 16:59:29 +0000

    👫👀 Checkpoint ./commands/ping.js:16007407/28

--- commands/ping.js
+++ commands/ping.js
@@ -1,37 +1,37 @@
 const Discord = require('discord.js');
-const ayarlar = require('../ayarlar.json');
-​
+const ayarlar = require('../config.json');
+
 exports.run = async (client, message, params) => {
   
   let prefix = ayarlar.prefix
  
   if (!params[0]) {
     message.channel.sendCode("asciidoc",`= KU-PİBOT Yardım Menüsü =
-​
+
 ${prefix}eğlence        ::  Eğlence komutlarını gösterir.
 ${prefix}kullanıcı      ::  Kullanıcı komutlarını gösterir.
 ${prefix}yetkili        ::  Yetkili komutlarını gösterir.
 ${prefix}ekstra         ::  Ekstra komutları gösterir.
 ${prefix}müzik          ::  Müzik komutlarını gösterir.
-​
+
 # Komutlar hakkında yardım almak icin ${prefix}yardım <komut ismi>`);
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
       message.channel.sendCode('asciidoc', `= ${command.help.name} =
-​
+
 Hakkında  :: ${command.help.description}
 Kullanım  :: ${prefix}${command.help.usage}`);
     }
   }
   
 };
-​
+
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ["yardım"],
   permLevel: 0
 };
 
