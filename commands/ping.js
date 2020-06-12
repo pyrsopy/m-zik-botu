98c8a194f08dc22347421f2bd09f8bae3dcaf078
Glitch (rythmgold) <none>
2020-06-12 16:22:45 +0000

    🌲🎥 Checkpoint ./commands/ping.js:16007407/3526

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,42 @@
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+​
+exports.run = async (client, message, params) => {
+  
+  let prefix = ayarlar.prefix
+ 
+  if (!params[0]) {
+    message.channel.sendCode("asciidoc",`= KU-PİBOT Yardım Menüsü =
+​
+${prefix}eğlence        ::  Eğlence komutlarını gösterir.
+${prefix}kullanıcı      ::  Kullanıcı komutlarını gösterir.
+${prefix}yetkili        ::  Yetkili komutlarını gösterir.
+${prefix}ekstra         ::  Ekstra komutları gösterir.
+${prefix}müzik          ::  Müzik komutlarını gösterir.
+​
+# Komutlar hakkında yardım almak icin ${prefix}yardım <komut ismi>`);
+  } else {
+    let command = params[0];
+    if (client.commands.has(command)) {
+      command = client.commands.get(command);
+      message.channel.sendCode('asciidoc', `= ${command.help.name} =
+​
+Hakkında  :: ${command.help.description}
+Kullanım  :: ${prefix}${command.help.usage}`);
+    }
+  }
+  
+};
+​
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
 
-module.exports = {
-  name: "yardım",
-  description: "Pinging the bot",
-  execute(client, message) {
-  channel.send('<a:wuw:710205330625396868> | ``Rythm Gold Komut Menüsü``  ')
+exports.help = {
+  name: 'yardım',
+  description: 'Komut kategorilerini gösterir.',
+  usage: 'yardım'
+};
