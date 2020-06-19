5118f9de317588610e9816c443c45c0ad456c9f7
Glitch (rythmgold) <none>
2020-06-19 16:23:30 +0000

    🌴🎈 Checkpoint ./commands/ping.js:16007407/3509

--- commands/ping.js
+++ commands/ping.js
@@ -4,15 +4,6 @@ module.exports = {
   description: "Pinging the bot",
   execute(client, message) {
   message.channel.send('<a:wuw:710205330625396868> | ``Rythm Gold Komut Menüsü``  ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -play = Müziği Başlatmanızı Sağlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -döngü = Müziği Tekrarlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -lyrics = Müziğin Sözlerini İnternetten Arar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -şarkı-adı = Müziği Başlatmanızı Sağlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -durdur = Müziği Durdurmamızı Sağlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -sıra = Müzik Sırasını Göstermeyi Sağlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -devam = Müziği Durdurduktan Sonra Başlatmayı Sağlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -atla = Müziği Atlar ')
-  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -leave = Müziği Bitirmeyi Sağlar ')
-    
+  message.channel.send('<a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -play = Müziği Başlatmanızı Sağlar \n <a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -döngü = Müziği Tekrarlar \n <a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -lyrics = Müziğin Sözlerini İnternetten Arar \n <a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -şarkı-adı = Şarkının Adını Yazar  \n <a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -durdur = Müziği Durdurmamızı Sağlar \n <a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -sıra = Müzik Sırasını Göstermeyi Sağlar \n <a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -devam = Müziği Durdurduktan Sonra Başlatmayı Sağlar \n <a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -atla = Müziği Atlar \n <a:rok2:710205094225772596>  | <a:wuw:710205330625396868>  | -leave = Müziği Bitirmeyi Sağlar  ')    
 }
 }
