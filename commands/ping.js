c543f8dad5afd312a85629f9f731bd6fbfeefe9c
Glitch (rythmgold) <none>
2020-06-19 15:47:18 +0000

    🐮🍄 Checkpoint ./config.json:16007407/316 ./commands/ping.js:16007407/10248

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "yardım",
   description: "Pinging the bot",
   execute(client, message) {
-  message.channel.send('Yardım Menüsüne Hoşgeldin :)')
+  message.channel.send('Yardım Menüsüne Hoşgeldin :) \n ')
 
 
 }
