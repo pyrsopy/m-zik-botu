e95a16552d6c39222aa5a3cdf10c7967a00c39d1
Glitch (rythmgold) <none>
2020-08-20 07:57:36 +0000

    ⏲🍃 Checkpoint ./config.json:21289958/59 ./server.js:21289958/48

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { TOKEN, PREFIX } = require("./config.json")
 //CLIENT EVENTS
 client.on("ready", () => {
   console.log('Ready TO play some soft songs')
-  client.user.setActivity("!yardım | Canbey Rythm Altyapısı ! ")
+  client.user.setActivity("!yardım | HızlıBilgi ! ")
 })
 
 client.on("warn", info => console.log(info));
