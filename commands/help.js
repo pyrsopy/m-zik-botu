84f94f03a4ef707d4549bc1da16a5704c599ff02
Glitch (rythmgold) <none>
2020-08-23 12:00:58 +0000

    ✨🐩 Checkpoint ./commands/help.js:21431008/3

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,5 @@ module.exports = {
   description: "yardım",
   execute(client, message) {
   message.channel.send(' | ``Rythm Gold Komut Menüsü``  ')
-  message.channel.send('!play \n Şarkı Çalıştırır \n !komutismi \n Bu Komudu Çalı')    
-}
+  message.channel.send('!play \n Şarkı Çalıştırır \n !komutismi \n Bu Komudu Çalı')    }
 }
