306507f4af7c88d07355a0bdf518efc7ab573781
Glitch (ring-ordinary-windflower) <none>
2020-09-11 16:09:48 +0000

    Rewound to commit 525b2e10c438437026b7f6593904b3a64041387d + 0 edits

--- commands/yardım.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  name: "yardım",
-  description: "yardım",
-  execute(client, message) {
-  message.channel.send(' | ``Desert Goose Müzik Bot Menüsü!``  ')
-  message.channel.send('!çal \n Müzik Çalıştırır. \n !komutismi \n Bu Komudu Çalı')    }
-}
--- server.js
+++ server.js
@@ -1,1 +1,78 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var fs = require('fs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// init sqlite db
+var fs = require('fs');
+var dbFile = './.data/sqlite.db';
+var exists = fs.existsSync(dbFile);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(dbFile);
+
+// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
+db.serialize(function(){
+  if (!exists) {
+    db.run('CREATE TABLE Dreams (dream TEXT)');
+    console.log('New table Dreams created!');
+    
+    // insert default dreams
+    db.serialize(function() {
+      db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
+    });
+  }
+  else {
+    console.log('Database "Dreams" ready to go!');
+    db.each('SELECT * from Dreams', function(err, row) {
+      if ( row ) {
+        console.log('record:', row);
+      }
+    });
+  }
+});
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// endpoint to get all the dreams in the database
+// currently this is the only endpoint, ie. adding dreams won't update the database
+// read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
+app.get('/getDreams', function(request, response) {
+  db.all('SELECT * from Dreams', function(err, rows) {
+    response.send(JSON.stringify(rows));
+  });
+});
+
+// Add a dream
+app.post('/addDream', function(request, response){
+  console.log('add to dreams', request.body);
   
+  // remix this app and uncomment the lines below to add your dream to the database!
+  // db.run(`INSERT INTO Dreams (dream) VALUES ("${request.body.dream}")`, (error) => {
+  //   if(error){
+  //     response.send({message: 'error!'});
+  //   }else{
+  //     response.send({message: 'success'});
+  //   }
+  // }
+  // );
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
