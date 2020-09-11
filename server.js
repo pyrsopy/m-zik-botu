e115c8b118a3336fc13b779bb88bb60fe9259640
Glitch (ring-ordinary-windflower) <none>
2020-09-11 17:07:58 +0000

    💫👚 Checkpoint ./server.js:16829938/4689

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const { TOKEN, PREFIX } = require("./config.json")
 //CLIENT EVENTS
 client.on("ready", () => {
   console.log('Ready TO play some soft songs')
-  client.user.setActivity("!yardım |MitaS DJ")
-  client.user.setActivity("İnstagram/mita2334 ")
+  client.user.setActivity("!yardım |MitaS DJ! ")
+  client.user.setActivity("İnstagram/sadadasda ")
 })
 
 client.on("warn", info => console.log(info));
