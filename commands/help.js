2a9c1ae4ea51c5340543f806f81d7600db0e2bda
Glitch (ring-ordinary-windflower) <none>
2020-09-11 16:11:06 +0000

    Rewound to commit f26b76b54ad64afc357b23d9b11fcc0d7aa8dc5a + 0 edits

---
+++ commands/help.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  name: "yardım",
+  description: "yardım",
+  execute(client, message) {
+  message.channel.send(' | ``Rythm Gold Komut Menüsü``  ')
+  message.channel.send('!play \n Şarkı Çalıştırır \n !komutismi \n Bu Komudu Çalı')    
+}
+}
--- server.js
+++ server.js
@@ -1,78 +1,61 @@
-// server.js
-// where your node app starts
+const discord = require("discord.js")
+const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { TOKEN, PREFIX } = require("./config.json")
 
-// init project
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var fs = require('fs');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+//CLIENT EVENTS
+client.on("ready", () => {
+  console.log('Ready TO play some soft songs')
+  client.user.setActivity("!yardım | Canbey Rythm Altyapısı ! ")
+})
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+client.on("warn", info => console.log(info));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+client.on("error", console.error)
 
-// init sqlite db
-var fs = require('fs');
-var dbFile = './.data/sqlite.db';
-var exists = fs.existsSync(dbFile);
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(dbFile);
+//DEFINIING
+client.commands = new discord.Collection()
+client.prefix = PREFIX
+client.queue = new Map();
 
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-db.serialize(function(){
-  if (!exists) {
-    db.run('CREATE TABLE Dreams (dream TEXT)');
-    console.log('New table Dreams created!');
-    
-    // insert default dreams
-    db.serialize(function() {
-      db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
-    });
-  }
-  else {
-    console.log('Database "Dreams" ready to go!');
-    db.each('SELECT * from Dreams', function(err, row) {
-      if ( row ) {
-        console.log('record:', row);
-      }
-    });
-  }
-});
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+//LETS LOAD ALL FILES
+const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"))
+for (const file of cmdFiles) {
+  const command = require(join(__dirname, "commands", file))
+  client.commands.set(command.name, command)
+} //LOADING DONE
 
-// endpoint to get all the dreams in the database
-// currently this is the only endpoint, ie. adding dreams won't update the database
-// read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
-app.get('/getDreams', function(request, response) {
-  db.all('SELECT * from Dreams', function(err, rows) {
-    response.send(JSON.stringify(rows));
-  });
-});
 
-// Add a dream
-app.post('/addDream', function(request, response){
-  console.log('add to dreams', request.body);
+//WHEN SOMEONE MESSAGE
+client.on("message", message => {
+   if (message.author.bot) return;
+  if (!message.guild) return;
   
-  // remix this app and uncomment the lines below to add your dream to the database!
-  // db.run(`INSERT INTO Dreams (dream) VALUES ("${request.body.dream}")`, (error) => {
-  //   if(error){
-  //     response.send({message: 'error!'});
-  //   }else{
-  //     response.send({message: 'success'});
-  //   }
-  // }
-  // );
+  if(message.content.startsWith(PREFIX)) { //IF MESSSAGE STARTS WITH MINE BOT PREFIX
+    
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/) //removing prefix from args
+    const command = args.shift().toLowerCase();
+    
+    if(!client.commands.has(command)) {
+      return;
+    } 
+    
+  try  { //TRY TO GET COMMAND AND EXECUTE
+      client.commands.get(command).execute(client, message, args)
+    } catch (err) { //IF IT CATCH ERROR
+      console.log(err)
+      message.reply("I am getting error on using this command")
+    }
+    
+  }
+  
+  
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+
+
+
+//DONT DO ANYTHING WITH THIS TOKEN lol
+client.login(TOKEN)
