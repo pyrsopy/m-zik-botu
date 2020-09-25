117f2cb42691bd82d9e687a1d434fa9055fbf273
Glitch (ring-ordinary-windflower) <none>
2020-09-25 01:43:52 +0000

    🔮🍍 Checkpoint ./config.json:3758578/102 ./commands/help.js:3758578/9

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports = {
   name: "yardım",
   description: "yardım",
   execute(client, message) {
-  message.channel.send(' ```Dj MitaS Komut Menüsü```  ')
+  message.channel.send(' ```Komut Menüsü```  ')
   message.channel.send('!oynat \n Şarkı Çalıştırır \n !yardım \n Bu Komudu Çalıştır.')    
 }
 }
