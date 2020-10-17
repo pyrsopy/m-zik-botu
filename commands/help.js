b5412514d88c850fd72048304d975067480f006d
Glitch (ring-ordinary-windflower) <none>
2020-10-17 11:30:35 +0000

    🎓🚟 Checkpoint ./config.json:23833337/200 ./commands/help.js:23833337/152 ./server.js:23833337/6

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,6 @@ module.exports = {
   description: "yardım",
   execute(client, message) {
   message.channel.send(' ```Komut Menüsü```  ')
-  message.channel.send(' ``mo!oynat= Şarkı Çalıştır``   ``mo!geç= şarkıyı geç``  ``mo!durdur= şarkıyı durdurur``   ``mo!döngü= şarkıyı tekrar ettirir``  ``mo!kuyruk= Sıraya eklediğiniz şarkıları gösterir`` ')    
+  message.channel.send(' ``mo!oynat= Şarkı Çalıştır`` ')    
 }
 }
